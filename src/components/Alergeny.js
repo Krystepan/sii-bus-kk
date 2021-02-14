3bf8a9bf4b8e6b26ac72bcd0d9d17be1c830ce67
Krystepan <krystian.konias@gmail.com>
2021-02-14 09:20:17 +0100

    2 files modified

--- src/components/Alergeny.js
+++ src/components/Alergeny.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Alergeny = () => {
-  return <h1>Alergeny</h1>;
+  return (
+    <form>
+      <fieldset>
+        <label for="alergen1">Alergen</label>
+        <input id="alergen1" name="alergen" type="text" />
+        <input type="button" value="usuń" />
+      </fieldset>
+      <button type="submit">Dodaj alergen</button>
+      <button type="submit">Zapisz</button>
+    </form>
+  );
 };
 
 export default Alergeny;
--- src/pages/MojProfil.js
+++ src/pages/MojProfil.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Components from "../components";
+
+const { PreferencjeZywieniowe, Alergeny } = Components;
 
 const MojProfil = () => {
-  return <h1>MojProfil</h1>;
+  return (
+    <>
+      <h1>MojProfil</h1>
+      <Alergeny />
+      <PreferencjeZywieniowe />
+    </>
+  );
 };
 
 export default MojProfil;
